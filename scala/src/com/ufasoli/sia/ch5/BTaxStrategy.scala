fc8b6a149e3be1a02103fdee908ba630f7849dc0
Ulises Fasoli <ufasoli@gmail.com>
2013-08-30 11:15:25 +0200

    work on CH5

---
+++ scala/src/com/ufasoli/sia/ch5/BTaxStrategy.scala
@@ -0,0 +1,12 @@
+package com.ufasoli.sia.ch5
+
+/**
+  *
+  * User: ufasoli
+  * Date: 28/08/13
+  * Time: 14:25
+  * project : scala-sia
+  */
+class BTaxStrategy extends TaxStrategy{
+   def taxIt(product: String): Double = ???
+ }
