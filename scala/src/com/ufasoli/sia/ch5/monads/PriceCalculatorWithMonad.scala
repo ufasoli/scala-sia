803db90a2400a32ce660650e784425f2f8cf10e5
Ulises Fasoli <ufasoli@gmail.com>
2013-09-03 17:42:09 +0200

    P 159

--- scala/src/com/ufasoli/sia/ch5/monads/PriceCalculatorWithMonad.scala
+++ scala/src/com/ufasoli/sia/ch5/monads/PriceCalculatorWithMonad.scala
@@ -19,6 +19,10 @@ package com.ufasoli.sia.ch5.monads
  */
 object PriceCalculatorWithMonad {
 
+
+  import Stubs._
+  import StateMonad.State._
+
   case class PriceState(productId:String, stateCode:String, price:Double)
 
 
@@ -29,16 +33,74 @@ object PriceCalculatorWithMonad {
     def calculateTax(productId: String, price: Double) = 5.0
   }
 
-  import Stubs._
+
   def findBasePrice(ps:PriceState):Double={
     val basePrice  = findTheBasePrice(ps.productId)
     basePrice
 
   }
 
-  def applyStateSpecificDiscount(ps:PriceState):Double = {
+  def applyStateSpecificDiscount(ps: PriceState): Double = {
     val discount = findStateSpecificDiscount(ps.productId, ps.stateCode)
     ps.price - discount
   }
 
+  def applyProductSpecificDiscount(ps: PriceState): Double = {
+    val discount = findProductSpecificDiscount(ps.productId)
+    ps.price - discount
+  }
+  def applyTax(ps: PriceState): Double = {
+    val tax = calculateTax(ps.productId, ps.price)
+    ps.price + tax
+  }
+
+
+  def modifyPriceState(f: PriceState => Double) =
+
+  modify[PriceState](s => s.copy(price = f(s)))
+  val stateMonad = for {
+
+    _ <- modifyPriceState(findBasePrice)
+    _ <- modifyPriceState(applyStateSpecificDiscount)
+    _ <- modifyPriceState(applyProductSpecificDiscount)
+    _ <- modifyPriceState(applyTax)
+  }yield ()
+
+
+
+
+}
+
+object StateMonad{
+  /**
+   * The state monad trait
+   * @tparam S
+   * @tparam A
+   */
+  trait State[S, +A]{
+
+    import State._
+
+    def apply(s:S):(S,A)
+    def map[B](f:A => B):State[S,B] = state(apply(_) match{
+      case (s,a) => (s, f(a))
+    })
+
+    def flatMap[B](f:A => State[S,B]): State[S,B]=
+    state(apply(_) match {
+      case (s,a) =>  f(a)(s)
+    })
+  }
+
+  object State{
+
+    def state[S,A](f:S => (S,A))= new State[S,A]{
+      def apply(s:S)= f(s)
+    }
+
+    def init[S]: State[S,S] = state[S,S](s => (s,s))
+
+    def modify[S] (f: S => S) =
+      init[S] flatMap (s => state(_ => (f(s), ())))
+  }
 }
