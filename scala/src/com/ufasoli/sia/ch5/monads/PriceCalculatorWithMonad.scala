83f3291e86b3d9cb6910df63db5c600a652bcf34
Ulises Fasoli <ufasoli@gmail.com>
2013-10-08 14:00:06 +0200

    added sbt project for chapter 6

--- scala/src/com/ufasoli/sia/ch5/monads/PriceCalculatorWithMonad.scala
+++ scala/src/com/ufasoli/sia/ch5/monads/PriceCalculatorWithMonad.scala
@@ -71,36 +71,24 @@ object PriceCalculatorWithMonad {
 
 }
 
-object StateMonad{
-  /**
-   * The state monad trait
-   * @tparam S
-   * @tparam A
-   */
-  trait State[S, +A]{
-
-    import State._
-
-    def apply(s:S):(S,A)
-    def map[B](f:A => B):State[S,B] = state(apply(_) match{
-      case (s,a) => (s, f(a))
+object StateMonad {
+  import State._
+  trait State[S, +A] {
+    def apply(s: S): (S, A)
+    def map[B](f: A => B): State[S, B] = state(apply(_) match {
+      case (s, a) => (s, f(a))
     })
-
-    def flatMap[B](f:A => State[S,B]): State[S,B]=
-    state(apply(_) match {
-      case (s,a) =>  f(a)(s)
-    })
+    def flatMap[B](f: A => State[S, B]): State[S, B] =
+      state(apply(_) match {
+        case (s, a) => f(a)(s)
+      })
   }
-
-  object State{
-
-    def state[S,A](f:S => (S,A))= new State[S,A]{
-      def apply(s:S)= f(s)
+  object State {
+    def state[S, A](f: S => (S, A)) = new State[S, A] {
+      def apply(s: S) = f(s)
     }
-
-    def init[S]: State[S,S] = state[S,S](s => (s,s))
-
-    def modify[S] (f: S => S) =
+    def init[S]: State[S, S] = state[S, S](s => (s, s))
+    def modify[S](f: S => S) =
       init[S] flatMap (s => state(_ => (f(s), ())))
   }
 }
